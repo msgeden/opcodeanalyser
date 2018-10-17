d290a9f42fa0795345915176a24af06ebd286a63
munden <munden@munden-OptiPlex-7050>
2018-10-17 17:37:30 +0100

    msic

--- src/opcodeanalyser/ObjdumpAnalyser.java
+++ src/opcodeanalyser/ObjdumpAnalyser.java
@@ -78,44 +78,7 @@ public class ObjdumpAnalyser {
 
 		}
 	}
-	public static void printStatistics(Output objectFile) throws IOException {
+	public static void createCFG(Output objectFile) throws IOException {
 
-		File instructionStatsFile = new File(FileHandler.readConfigValue(Definitions.STATS_PATH) + "memory_control_"
-				+ objectFile.getFileName().replaceAll("txt", "tsv"));
-		FileUtils.deleteQuietly(instructionStatsFile);
-		FileUtils.write(instructionStatsFile,
-				"FUNCTION" + Definitions.TAB_CHAR + "CONTROL_TRANSFERS" + Definitions.TAB_CHAR + "MEMORY_READ"
-						+ Definitions.TAB_CHAR + "MEMORY_WRITE" + Definitions.TAB_CHAR + "OTHER" + Definitions.TAB_CHAR
-						+ "TOTAL",
-				Charset.defaultCharset(), true);
-		System.out.print("FUNCTION" + Definitions.TAB_CHAR + "CONTROL_TRANSFERS" + Definitions.TAB_CHAR + "MEMORY_READ"
-				+ Definitions.TAB_CHAR + "MEMORY_WRITE" + Definitions.TAB_CHAR + "OTHER" + Definitions.TAB_CHAR
-				+ "TOTAL");
-
-		for (BasicBlock block : objectFile.getFunctions()) {
-			block.countInstructions();
-			FileUtils.write(instructionStatsFile,
-					Definitions.NEW_LINE + block.getBlockName() + Definitions.TAB_CHAR + block.getControlCount()
-							+ Definitions.TAB_CHAR + block.getReadCount() + Definitions.TAB_CHAR + block.getWriteCount()
-							+ Definitions.TAB_CHAR + block.getOtherCount() + Definitions.TAB_CHAR
-							+ block.getTotalCount(),
-					Charset.defaultCharset(), true);
-			System.out.print(Definitions.NEW_LINE + block.getBlockName() + Definitions.TAB_CHAR
-					+ block.getControlCount() + Definitions.TAB_CHAR + block.getReadCount() + Definitions.TAB_CHAR
-					+ block.getWriteCount() + Definitions.TAB_CHAR + block.getOtherCount() + Definitions.TAB_CHAR
-					+ block.getTotalCount());
-
-		}
-		objectFile.countInstructions();
-		FileUtils.write(instructionStatsFile,
-				Definitions.NEW_LINE + objectFile.getFileName() + Definitions.TAB_CHAR + objectFile.getControlCount()
-						+ Definitions.TAB_CHAR + objectFile.getReadCount() + Definitions.TAB_CHAR
-						+ objectFile.getWriteCount() + Definitions.TAB_CHAR + objectFile.getOtherCount()
-						+ Definitions.TAB_CHAR + objectFile.getTotalCount(),
-				Charset.defaultCharset(), true);
-		System.out.print(Definitions.NEW_LINE + objectFile.getFileName() + Definitions.TAB_CHAR
-				+ objectFile.getControlCount() + Definitions.TAB_CHAR + objectFile.getReadCount() + Definitions.TAB_CHAR
-				+ objectFile.getWriteCount() + Definitions.TAB_CHAR + objectFile.getOtherCount() + Definitions.TAB_CHAR
-				+ objectFile.getTotalCount());
 	}
 }
